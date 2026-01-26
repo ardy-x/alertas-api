525097eaf020f3ce31e6c9e286bab09f6cc86f40
Ruben David <ruben.mamani.ali@policia.bo>
2026-01-25 22:17:30 -0400

    fix(decoradores): corrige el retorno del ID de usuario en el decorador IdUsuarioActual

--- src/autenticacion/infraestructura/decoradores/id-usuario.decorator.ts
+++ src/autenticacion/infraestructura/decoradores/id-usuario.decorator.ts
@@ -2,5 +2,5 @@ import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
 
 export const IdUsuarioActual = createParamDecorator((_data: unknown, ctx: ExecutionContext): string => {
   const request = ctx.switchToHttp().getRequest();
-  return request.user.idUsuario;
+  return request.user.userId;
 });
