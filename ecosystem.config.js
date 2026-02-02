d3ae0b92184e1dd44b092a052fbdce7dc04c255b
Ruben David <ruben.mamani.ali@policia.bo>
2026-02-02 19:29:12 -0400

    feat(pm2): agrega scripts para gestión de procesos con PM2 en package.json

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'alertas-api',
+      script: './dist/src/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
